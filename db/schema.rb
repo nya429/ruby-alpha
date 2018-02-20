37d06b5d5300de86dbc1d770493f517e99c72a7d
yue <yue@yues-MacBook-Pro.local>
2018-02-20 14:20:04 -0500

    add debugger in development view, add user and articles association

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180220154236) do
+ActiveRecord::Schema.define(version: 20180220183945) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "create_at"
     t.datetime "update_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
