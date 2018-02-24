cca910284dcbe50455044581a8a6ba45a2a073bd
yue <yue@yues-MacBook-Pro.local>
2018-02-24 17:23:22 -0500

    add user articles and gravatar

--- app/controllers/concerns/users_controller.rb
+++ app/controllers/concerns/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  
+
   def create
     # debugger
     @user = User.new(user_params)
@@ -13,7 +13,25 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
- 
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:sucess] = "User information has been updated"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
