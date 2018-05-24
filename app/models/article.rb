df0107f13df971678ffb010fa8ec55f1e4c2fcf8
cloud9 <song1991yue@gmail.com>
2018-05-24 20:45:51 +0000

    add article category asssociation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,8 @@
 class Article < ActiveRecord::Base
+  #belongs_to take singular
   belongs_to :user
+  has_many :article_categories
+  has_many :categories, through: :article_categories
   validates :title, presence: true, length: {minimum: 10, maxmum: 50}
   validates :description, presence: true, length: {minimum: 10, maxmum: 300}
   validates :user_id, presence: true
