020d90ea8575306143041db44cfa780ccc4d182c
yue <yue@yues-MacBook-Pro.local>
2018-07-17 21:07:08 -0400

    update category edit view

--- app/controllers/concerns/categories_controller.rb
+++ app/controllers/concerns/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :require_admin, except: [:index, :show]
+  before_action :set_category, only: [:edit, :update, :show, :destroy]
 
   def index
      @categories = Category.paginate(page: params[:page], per_page: 5)
@@ -20,12 +21,27 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @category.update(category_params)
+      flash[:success] = "Category was successfully edited"
+      redirect_to category_path(@category)
+    else
+      render 'edit'
+    end
+  end
+
   def show
-    @category = Category.find(params[:id])
     @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
 
   private
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
   def category_params
     params.require(:category).permit(:name)
   end
